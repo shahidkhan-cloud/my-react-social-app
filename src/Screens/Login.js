8eb814f1ba31638f0a4b160b8f8d29a3ec0fd49d
shahidkhan-cloud <shahidsultankhail@gmail.com>
2025-10-17 12:51:05 +0500

    hhhh

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -20,7 +20,7 @@ const Login = () => {
       setLoading(true);
 
       // ✅ API call to backend login route
-      const res = await axios.post("https://my-react-social-app-backend.vercel.com/api/auth/login", {
+      const res = await axios.post("https://https://my-react-social-app-backend-dtyc.vercel.app/api/auth/login", {
         username,
         password,
       });
